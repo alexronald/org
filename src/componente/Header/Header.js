febaf1936626d94e99e8d1ad1a59ad4f49ddad8a
Alex Andrade <alexronald43@gmail.com>
2023-04-27 23:07:41 -0500

    completo

--- src/componente/Header/Header.js
+++
@@ -1,9 +0,0 @@
-import "./Header.css"
-
-function Header(){
-    return <header className="Header">
-        <img src='/img/HeaderHeader_total.png' alt='org'></img>
-    </header>
-}
-
-export default Header;
